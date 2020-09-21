1f97280fe0f7cbdfeb36ab7b138bbf4a43441759
michael <mickwald@hotmail.com>
2020-09-21 17:38:39 +0200

    Possibly fucked vector lab

--- vector/Vector.cc
+++ vector/Vector.cc
@@ -1,45 +1,88 @@
-#include "Vector.h"
+#include <cmath>
+#include <iostream>
 
 // implement all the functions declared in Vector.h here
 class Vector{
-	Vector(double x, double y) : x(x), y(y);
-	Vector() : {x{}; y{};};
+	public:
+	Vector() : x{}, y{} {}
+	Vector(double x, double y) : x(x), y(y) {}
+	Vector(Vector const& vec) : x(vec.x), y(vec.y) {}
 	private:
 	double x,y;
 	
 	public:
-	Vector& operator!(){
-		return Vector(-this.x , -this.y );
-	}
-	
 	double getX(){
-		return this.x;
+		return x;
 	}
 	double getY(){
-		return this.y;
+		return y;
 	}
 	
-	Vector& operator+(Vector& other){
-		return Vector(this.getX()+other.getX(), this.getY()+other.getY());
+	Vector& operator=(Vector const& vec){
+		if(this != &vec){
+			x = vec.x;
+			y = vec.y;
+		}
+		return *this;
 	}
 	
-	Vector& operator-(Vector& other){
-		return Vector(this.getX()-other.getX(), this.getY()-other.getY());
+	Vector& operator+=(Vector const& vec){
+		x += vec.x;
+		y += vec.y;
+		return *this;
 	}
 	
-	Vector& operator*(double scalar){
-		return Vector(this.x * scalar,this.y * scalar);
+	friend Vector operator+(Vector left, Vector const& right){
+		left += right;
+		return left;
 	}
 	
-	bool operator== (Vector& other){
-		return if(this.x == other.x && this.y == other.y)
+	friend Vector operator-(Vector left, Vector const& other){
+		return Vector(left.x-other.x, left.y-other.y);
+	}
+	
+	Vector operator-(){
+		return Vector(-x, -y);
+	}
+	
+	friend Vector operator*(Vector vec, double scalar){
+		return Vector(vec.x * scalar,vec.y * scalar);
+	}
+	
+	friend Vector operator*(double scalar, Vector const& vec){
+		return vec*scalar;
+	}
+	
+	friend bool operator== (Vector const& left, Vector const& right){
+		return (left.x == right.x && left.y == right.y);
 	}
 
-	bool operator!= (Vector& other){
-		return !(this == other);
+	friend bool operator!= (Vector const& left, Vector const& right){
+		return !(left == right);
 	}
 	
-	Vector& operator/ (double scalar){
-		return Vector(this.x/scalar, this.y/scalar);
+	Vector operator!(){
+		return Vector(-x , -y );
+	}
+	
+	Vector operator/ (double scalar){
+		return Vector(x/scalar, y/scalar);
+	}
+	
+	double length(){
+		return (std::sqrt((std::pow(x,2.0)+std::pow(y,2.0))));
+	}
+	
+	friend std::ostream& operator<<(std::ostream& stream, Vector const& vec){
+		return stream << "( " << vec.x << ", " << vec.y << " )";
+	}
+	
+	
+	friend std::istream& operator>>(std::istream& stream, Vector vec){
+		Vector tmp;
+		stream >> tmp.x;
+		stream >> tmp.y;
+		vec = tmp;
+		return stream;
 	}
 };
--- vector/Vector.h
+++ vector/Vector.h
@@ -1,18 +1,27 @@
 #pragma once
+#include <iostream>
 
 // Declare your class here
 class Vector{
 	public:
-	Vector(double x, double y);
 	Vector();
-	Vector& operator!();
+	Vector(double x, double y);
+	Vector(Vector const& vec);
 	double getX();
 	double getY();
-	Vector& operator+(Vector& vec);
-	Vector& operator-(Vector& vec);
-	Vector& operator*(Vector& vec);
-	bool operator==(Vector& vec);
-	bool operator!=(Vector& vec);
-	Vector& operator/(Vector& vec);
+	Vector& operator=(Vector const& vec);
+	Vector& operator+=(Vector const& vec);
+	friend Vector operator+(Vector left, Vector const& right);
+	friend Vector operator-(Vector left, Vector const& right);
+	Vector& operator-();
+	friend Vector operator*(Vector const& vec, double scalar);
+	friend Vector operator*(double scalar, Vector const& vec);
+	friend bool operator==(Vector const & left, Vector const& right);
+	friend bool operator!=(Vector const & left, Vector const& right);
+	Vector operator!();
+	Vector operator/(double scalar);
+	double length();
+	friend std::ostream& operator<<(std::ostream& stream, Vector const& vec);
+	friend std::istream& operator>>(std::istream& stream, Vector vec);
 	
 };
